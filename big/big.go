02008873d4c24c5dd776e4594dbc37075ada98d1
Abner Chen <houjunchen@gmail.com>
2018-08-06 21:19:09 +0800

    Add big

---
+++ big/big.go
@@ -0,0 +1,22 @@
+// https://golang.org/pkg/math/big/#example_Int_Scan
+package main
+
+import (
+	"fmt"
+	"log"
+	"math/big"
+)
+
+func main() {
+	// The Scan function is rarely used directly;
+	// the fmt package recognizes it as an implementation of fmt.Scanner.
+	i := new(big.Int)
+	_, err := fmt.Sscan("0x5b5ff4eeba3614f594f40b2a", i)
+	if err != nil {
+		log.Println("error scanning value:", err)
+	} else {
+		fmt.Println(i)
+	}
+	is := fmt.Sprintln(i)
+	fmt.Println(is)
+}
